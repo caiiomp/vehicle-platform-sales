df2f83303a043f4e79d2990af594b06ca570db49
Caio <caiomp87@gmail.com>
2025-11-27 22:32:17 -0300

    add cloud sql support (#14)

--- src/main.go
+++ src/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
 
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/lib/pq"
 	swaggerFiles "github.com/swaggo/files"
@@ -27,21 +32,23 @@ import (
 
 func main() {
 	var (
-		apiPort = os.Getenv("API_PORT")
+		ctx = context.Background()
 
-		host     = os.Getenv("DB_HOST")
-		port     = os.Getenv("DB_PORT")
-		user     = os.Getenv("DB_USER")
-		password = os.Getenv("DB_PASSWORD")
-		dbname   = os.Getenv("DB_NAME")
+		environment = os.Getenv("ENVIRONMENT")
+		apiPort     = os.Getenv("API_PORT")
+
+		instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME")
+		host                   = os.Getenv("DB_HOST")
+		port                   = os.Getenv("DB_PORT")
+		user                   = os.Getenv("DB_USER")
+		password               = os.Getenv("DB_PASSWORD")
+		dbname                 = os.Getenv("DB_NAME")
 
 		vehiclePlatformPaymentsHost = os.Getenv("VEHICLE_PLATFORM_PAYMENTS_HOST")
 		vehiclePlatformSalesHost    = os.Getenv("VEHICLE_PLATFORM_SALES_HOST")
 	)
 
-	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", dataSourceName)
+	db, err := getDb(ctx, environment, instanceConnectionName, host, port, user, password, dbname)
 	if err != nil {
 		log.Fatalf("error to connect database: %s", err)
 	}
@@ -82,6 +89,53 @@ func main() {
 	if err = app.Run(":" + apiPort); err != nil {
 		log.Fatalf("coult not initialize http server: %v", err)
 	}
+}
+
+func getDb(ctx context.Context, environment, instanceConnectionName, host, port, user, password, dbname string) (*sql.DB, error) {
+	var (
+		db *sql.DB
+
+		config *pgx.ConnConfig
+		dialer *cloudsqlconn.Dialer
+		opts   []cloudsqlconn.Option
+
+		dataSourceName string
+		err            error
+	)
+
+	switch environment {
+	case "PROD":
+		dataSourceName = fmt.Sprintf("user=%s password=%s database=%s", user, password, dbname)
+
+		config, err = pgx.ParseConfig(dataSourceName)
+		if err != nil {
+			return nil, err
+		}
+
+		opts = append(opts, cloudsqlconn.WithLazyRefresh())
+
+		dialer, err = cloudsqlconn.NewDialer(ctx, opts...)
+		if err != nil {
+			return nil, err
+		}
+
+		config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+			return dialer.Dial(ctx, instanceConnectionName)
+		}
+
+		dbUri := stdlib.RegisterConnConfig(config)
+		db, err = sql.Open("pgx", dbUri)
+
+	default:
+		dataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+		db, err = sql.Open("postgres", dataSourceName)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func timeGenerator() time.Time {
